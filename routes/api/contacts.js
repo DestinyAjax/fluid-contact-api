2fe9461ede0397f5e71968fab2ee29ee7d94e411
Destiny Ajax <destinyajakaiye@gmail.com>
2018-10-11 17:18:57 +0100

    Implemented view all user contacts | Implemented creating new contact | Implemented updating a single contact

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,8 +8,13 @@ router.post('/', auth.required, (request, response, next) => {
 });
 
 /** Connecting the signin route to the userController */
-router.get('/', auth.required, (request, response) => {
-  ContactController.allContact(request, response);
+router.get('/:id', auth.required, (request, response, next) => {
+  ContactController.allContacts(request, response, next);
+});
+
+/** this route is responsible for updating a single contact */
+router.patch('/:id', auth.required, (request, response, next) => {
+  ContactController.updateContact(request, response, next);
 });
 
 module.exports = router;
