a49cd17183f4c55a8917a63828d91a691ae0188c
Destiny Ajax <destinyajakaiye@gmail.com>
2018-10-11 16:16:49 +0100

    Implemented user signin | Implemented user signup | Implemented user signout

--- migrations/20181011025905-create-contact.js
+++ migrations/20181011025905-create-contact.js
@@ -20,6 +20,9 @@ module.exports = {
       email: {
         type: Sequelize.STRING
       },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- models/contact.js
+++ models/contact.js
@@ -1,13 +1,24 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Contact = sequelize.define('Contact', {
-    fullname: DataTypes.STRING,
-    address: DataTypes.STRING,
-    telephone: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
-  Contact.associate = function(models) {
-    // associations can be defined here
-  };
-  return Contact;
+const DataTypes = require('sequelize');
+const User = require('../models/user');
+const sequelize = require('../config/db.config');
+
+const Contact = sequelize.define('Contact', {
+  fullname: DataTypes.STRING,
+  address: DataTypes.STRING,
+  telephone: DataTypes.STRING,
+  email: DataTypes.STRING,
+  user_id: {
+    type: DataTypes.INTEGER,
+    reference: {
+      model: User,
+      key: 'id'
+    }
+  }
+}, {});
+
+Contact.associate = function(models) {
+  // associations can be defined here
 };
+
+module.exports = Contact;
